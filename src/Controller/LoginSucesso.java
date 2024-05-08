f1689851e67be68cbad6652950fc00f001375bda
unifjgarcia <unifjgarcia@fei.edu.br>
2024-05-07 23:20:33 -0300

    adicao do login do investidor pelo cpf e senha e indo ao menu

---
+++ src/Controller/LoginSucesso.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import DAO.ConexaoBancoDados;
+import DAO.InvestidorConectado;
+import Model.Investidor;
+import View.Login;
+import View.Menu;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jpsab
+ */
+public class LoginSucesso {
+    private Login view;
+
+    public LoginSucesso(Login view) {
+        this.view = view;
+    }
+
+    public void LoginInvestidor(){
+        Investidor investidor = new Investidor(null,null, view.getTxtCPF().getText(),
+                                                          view.getTxtSenha().getText());
+        ConexaoBancoDados conectar = new ConexaoBancoDados();
+        try{
+            Connection conexao = conectar.getConnection();
+            InvestidorConectado conectado = new InvestidorConectado(conexao);
+            ResultSet resultado = conectado.infoInvestidor(investidor);
+            if(resultado.next()){
+                JOptionPane.showMessageDialog(view, "Login realizado com sucesso!");
+                Menu janelaMenu = new Menu();
+                janelaMenu.setVisible(true);
+                view.setVisible(false);
+            } else{
+                JOptionPane.showMessageDialog(view, "Seu cpf ou senha estão errados ou não cadastrados");
+            }
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(view, "Erro de conexão!");
+        }
+    }
+}
+        
+    
+
---
+++ src/DAO/InvestidorConectado.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+import Model.Investidor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jpsab
+ */
+public class InvestidorConectado {
+    private Connection conexao;
+
+    public InvestidorConectado(Connection conexao) {
+        this.conexao = conexao;
+    }
+    
+    public ResultSet infoInvestidor(Investidor investidor) throws SQLException{
+        String sql = "select * from investidor where cpf = ? and senha = ?";
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        //corrigindo problema injeção sql
+        statement.setString(1, investidor.getCpf());
+        statement.setString(2, investidor.getSenha());
+        statement.execute();
+        ResultSet resultado = statement.getResultSet();
+        return resultado;
+    }
+    
+    public void adicionaInvestidor(Investidor investidor) throws SQLException{
+        String sql = "insert into investidor (nome, idade, cpf, senha) values ('" +
+                        investidor.getNome() + "', '" +
+                        investidor.getIdade() + "', '" +
+                        investidor.getCpf() + "', '" +
+                        investidor.getSenha() + "')";
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        statement.execute();
+        conexao.close();
+    }
+    
+    public void excluirInvestidor(Investidor investidor) throws SQLException{
+        String sql = "delete from investidor where cpf = ? and senha = ?";
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        statement.setString(1, investidor.getCpf());
+        statement.setString(2, investidor.getSenha());
+        statement.execute();
+        conexao.close();
+    }
+}
